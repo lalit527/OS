9e9e0d589c3dafa996f8ee6cc3ae35598b020b7f
lalit527 <lalit.slg007@gmail.com>
2018-04-11 21:03:19 +0530

    Reader Writer Begining

---
+++ reader-writer/priority-reader-writer.h
@@ -0,0 +1,8 @@
+#ifndef PS1_PRIORITY_HEADER_WRITER_H
+#define PS1_PRIORITY_HEADER_WRITER_H
+
+int main(int argc, char **argv);
+void *rederMain(void *threadArgument);
+void *writerMain(void *threadArgument);
+
+#endif
