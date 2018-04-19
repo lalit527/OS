6f33f5f2180b2df9e8c7fdfdcf3e855d42e26be9
lalit527 <lalit.slg007@gmail.com>
2018-04-19 18:47:57 +0530

    Complete program for producer consumer using custom mutex

--- producer-consumer/producer-consumer-mutex.c
+++ producer-consumer/producer-consumer-mutex.c
@@ -3,30 +3,30 @@
 
 int temp = 0, overflow = 0, mutex = 1, underflow = 10;
 
-int wait_protocol(int counter) {
+int wait_thread(int counter) {
     return --counter;
 }
 
-int signal_protocol(int counter) {
+int signal_thread(int counter) {
     return ++counter;
 }
 
 void producer() {
-    mutex = wait_protocol(mutex);
-    overflow = signal_protocol(overflow);
-    underflow = wait_protocol(underflow);
+    mutex = wait_thread(mutex);
+    overflow = signal_thread(overflow);
+    underflow = wait_thread(underflow);
     ++temp;
     printf("\nProducer:\t Element %d\n", temp);
-    mutex = signal_protocol(mutex);
+    mutex = signal_thread(mutex);
 }
 
 void consumer() {
-    mutex = wait_protocol(mutex);
-    overflow = wait_protocol(overflow);
-    underflow = signal_protocol(underflow);
+    mutex = wait_thread(mutex);
+    overflow = wait_thread(overflow);
+    underflow = signal_thread(underflow);
     printf("\nConsumer:\t Element %d\n", temp);
     temp--;
-    mutex = signal_protocol(mutex);
+    mutex = signal_thread(mutex);
 }
 
 int main() {
